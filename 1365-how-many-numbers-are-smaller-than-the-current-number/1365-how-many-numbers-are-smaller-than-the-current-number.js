91d7faabb620aca1411be6f008351dfb071efd91
JishnuV2004 <jishnu3039@gmail.com>
2025-09-18 10:06:47 +0530

    Time: 11 ms (25.98%), Space: 56.5 MB (81.2%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
@@ -4,14 +4,17 @@
  */
 var smallerNumbersThanCurrent = function(nums) {
     let arr=[]
+       let count=0
     for (i of nums){
-        let count=0
         for(j of nums){
             if(i > j){
                 count++
+               
             }
+             
         }
         arr.push(count)
+        count=0
     }
     return arr
 };
