7a3f027727114e8944d116992b812d483514db0f
JishnuV2004 <jishnu3039@gmail.com>
2025-09-20 10:53:31 +0530

    Time: 17 ms (13.74%), Space: 55.7 MB (72.7%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -3,14 +3,11 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    let sort=nums.sort((a,b)=>a-b)
-    let uniq=0
-    console.log(sort)
+    let sort=nums.sort()
     for(let i=0; i<sort.length; i+=2){
         if(sort[i] !== sort[i+1]){
             return sort[i]
         }
     }
-    // console.log(uniq)
     return uniq
 };
