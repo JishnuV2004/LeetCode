eb25fdf191a7369aab8351884df378be0b0a373c
JishnuV2004 <jishnu3039@gmail.com>
2025-10-03 10:13:03 +0530

    Time: 44 ms (54.78%), Space: 53.6 MB (73.53%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} command
+ * @return {string}
+ */
+var interpret = function(command) {
+    let str=""
+    let i=0
+    while(i<command.length){
+        if(command[i]=="G"){
+            str+="G"
+            i++
+        }else if(command[i]=="(" && command[i+1]=="a"){
+            str+="al"
+            i+=4
+        }else{
+            str+="o"
+            i+=2
+        }
+    }
+    return str
+};
