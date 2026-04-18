931348cb01ab2df83b2e4e8080157bbc01eb161a
Jishnu V <jishnu3039@gmail.com>
2026-04-18 11:55:08 +0530

    Time: 0 ms (100%), Space: 4 MB (45.07%) - LeetHub

---
+++ 3783-mirror-distance-of-an-integer/3783-mirror-distance-of-an-integer.go
@@ -0,0 +1,13 @@
+func mirrorDistance(n int) int {
+    currentNvalue:=n
+    reversed := 0
+    for n != 0 {
+        reversed = reversed*10 + n%10
+        n/=10
+    }
+    sum := reversed - currentNvalue
+    if sum < 0 {
+        return -sum
+    }
+    return sum
+}
