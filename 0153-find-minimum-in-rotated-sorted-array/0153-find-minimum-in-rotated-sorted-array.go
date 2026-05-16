99fadd3aa19e586975f38c7a131150dbbc57cae6
Jishnu V <jishnu3039@gmail.com>
2026-05-16 10:10:10 +0530

    Time: 3 ms (0.33%), Space: 4.3 MB (38.62%) - LeetHub

---
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.go
@@ -0,0 +1,16 @@
+func findMin(nums []int) int {
+    for i:=0; i<len(nums); i++ {
+        swapped := false
+        for j:=0; j<len(nums)-1; j++ {
+            if nums[j] > nums[j+1] {
+                nums[j], nums[j+1] = nums[j+1], nums[j]
+                swapped = true
+            }
+            
+        }
+        if swapped != true {
+                break
+            }
+    }
+    return nums[0]
+}
