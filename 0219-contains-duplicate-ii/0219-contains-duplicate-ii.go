a389597123ab98db460aa5a1b0e4a8f4ec8a5254
Jishnu V <jishnu3039@gmail.com>
2026-05-22 11:05:59 +0530

    Time: 35 ms (53.92%), Space: 14 MB (42.91%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.go
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.go
@@ -2,14 +2,10 @@ func containsNearbyDuplicate(nums []int, k int) bool {
     freq := make(map[int]int)
     for i:=0; i<len(nums); i++ {
         _, ok := freq[nums[i]]
-        if !ok {
-            freq[nums[i]] = i
-            continue
-        } else if i - freq[nums[i]] <= k {
-            return true
-        } else {
-            freq[nums[i]] = i 
+        if ok && i - freq[nums[i]] <= k {
+            return true 
         }
+        freq[nums[i]] = i 
     }
     return false
 }
