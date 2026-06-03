ef2cc8ed7985ec4087e0fc03c9fce3ea6c7a4a9b
Jishnu V <jishnu3039@gmail.com>
2026-06-03 17:29:01 +0530

    Time: 0 ms (100%), Space: 6.9 MB (16.9%) - LeetHub

---
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.go
@@ -0,0 +1,16 @@
+func leftRightDifference(nums []int) []int {
+    leftSum := make([]int, len(nums))
+    rightSum := make([]int, len(nums))
+    output := make([]int, len(nums))
+
+    for i := range nums {
+        if i != 0{
+            leftSum[i] = leftSum[i-1] + nums[i-1]
+            rightSum[len(nums)-i-1] = rightSum[len(nums)-i] + nums[len(nums)-i]
+        }    
+    }
+    for i := range nums {
+        output[i] = int(math.Abs(float64(leftSum[i] - rightSum[i])))
+    }
+    return output
+}
