bfb0dd715f0564b9eebe73fba0a9af3091392d8c
Jishnu V <jishnu3039@gmail.com>
2026-06-25 09:40:44 +0530

    Time: 35 ms (24.73%), Space: 11.4 MB (33.51%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.go
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.go
@@ -2,10 +2,11 @@ func maxIceCream(costs []int, coins int) int {
  sort.Ints(costs) 
  res:=0
  for _,n:=range costs{
-    if coins-n >= 0{
-        coins-=n 
-        res++
+    if n > coins {
+      break
     }
+    coins-=n 
+    res++
  }   
  return res
 }
